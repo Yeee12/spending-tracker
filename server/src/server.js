deb237c5339717cf0fb181739cef3e8fa5bd7006
michael <michaelaina8@gmail.com>
2024-03-13 16:25:55 +0100

    nodemon

--- server/src/server.js
+++ server/src/server.js
@@ -3,8 +3,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer("app");
+const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, console.log(`Server running on port ${PORT}`)
+);
